850c86561e974a72f1eda7442a4eb1729c4fb392
yvmohith <147829086+yvmohtih@users.noreply.github.com>
2025-07-06 13:29:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail="mohithyv@gmail.com"
+var accountPassword = "1234"
+accountCity="Jaipur"
+let accountState;
+// accountId = 2
+accountEmail = "123@gmail.com"
+accountPassword="2121"
+accountCity="Mysore"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// array.forEach(element => {
+    // comment , scope
+// });
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) we are using nodejs,not browseer
+console.log(3+
+    3) //code readability should be good
+
+console.log("mohith")
+
+let name ="mohith"
+let age = 18
+let isLoggedIn=false
+let state;
+
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
