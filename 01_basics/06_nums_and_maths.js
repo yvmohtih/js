41516fd3075602e8a217254560665ebf09adba28
yvmohith <147829086+yvmohtih@users.noreply.github.com>
2025-07-06 17:10:09 +0000

    number and maths done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+
+// console.log((hundreds.toLocaleString('en-IN')));
+
+
+// +++++      Maths  ++++++
+console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.5));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
