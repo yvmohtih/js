3d3fce1e8f4a1865941220d6b7da2692a2579178
yvmohith <147829086+yvmohtih@users.noreply.github.com>
2025-07-06 13:41:00 +0000

    conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33ab" => NaN
+// true=>1;
+
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1=> true , 0=>false
+// ""=> false
+// "hitesh"=> true
+ 
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log( stringNumber)
+console.log(typeof stringNumber)
